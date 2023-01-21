34c121fb87e94b25554695cc4c8933b070bb6319
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-21 20:23:32 +0330

    add BaseServiceController class

--- src/main/java/ir/maktab/HomeServiceProvider/controller/BaseServiceController.java
+++ src/main/java/ir/maktab/HomeServiceProvider/controller/BaseServiceController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/baseService")
+@RequestMapping("/base-service")
 @RequiredArgsConstructor
 public class BaseServiceController {
 }
