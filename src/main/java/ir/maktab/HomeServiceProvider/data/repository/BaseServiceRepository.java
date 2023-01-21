3db85f3333b3fe637b12117f0b9e8fbdce6a3812
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-21 20:16:17 +0330

    add BaseServiceRepository Interface

--- src/main/java/ir/maktab/HomeServiceProvider/data/repository/BaseServiceRepository.java
+++ src/main/java/ir/maktab/HomeServiceProvider/data/repository/BaseServiceRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BaseServicesRepository extends JpaRepository<BaseService, Long> {
+public interface BaseServiceRepository extends JpaRepository<BaseService, Long> {
 }
