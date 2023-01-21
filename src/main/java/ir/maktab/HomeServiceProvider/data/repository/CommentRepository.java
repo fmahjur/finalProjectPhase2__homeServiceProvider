a55f887d008316a8a424f4b56e398ace3f986e7b
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-21 19:22:57 +0330

    add CommentRepository Interface

---
+++ src/main/java/ir/maktab/HomeServiceProvider/data/repository/CommentRepository.java
@@ -0,0 +1,8 @@
+package ir.maktab.HomeServiceProvider.data.repository;
+
+import ir.maktab.HomeServiceProvider.data.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+}
