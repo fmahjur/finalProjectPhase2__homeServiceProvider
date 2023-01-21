e7d860e913734a785c2920135520cfebe42469e4
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-21 20:15:47 +0330

    add SubServicesRepository Interface

---
+++ src/main/java/ir/maktab/HomeServiceProvider/data/repository/SubServiceRepository.java
@@ -0,0 +1,7 @@
+package ir.maktab.HomeServiceProvider.data.repository;
+
+import ir.maktab.HomeServiceProvider.data.model.SubService;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubServiceRepository extends JpaRepository<SubService, Long> {
+}
