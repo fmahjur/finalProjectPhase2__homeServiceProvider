b35b8e6ad5467bf383ebc1e6114cd6861749d0d9
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-25 00:21:54 +0330

    add BaseServiceValidator class

--- src/main/java/ir/maktab/HomeServiceProvider/validation/BaseServiceValidator.java
+++ src/main/java/ir/maktab/HomeServiceProvider/validation/BaseServiceValidator.java
@@ -1,18 +1,22 @@
 package ir.maktab.HomeServiceProvider.validation;
 
+import ir.maktab.HomeServiceProvider.data.repository.BaseServiceRepository;
 import ir.maktab.HomeServiceProvider.exception.ValidationException;
-import ir.maktab.HomeServiceProvider.model.dao.ServicesRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
-public class ServiceValidatior {
-    static ServicesRepository servicesRepository = ServicesRepository.getInstance();
+@Component
+@RequiredArgsConstructor
+public class BaseServiceValidator {
+    private static BaseServiceRepository baseServiceRepository;
 
     public static void isExistService(String serviceName) throws ValidationException {
-        if (servicesRepository.isExist(serviceName))
+        if (baseServiceRepository.isExist(serviceName))
             throw new ValidationException("this Service is already Exist!");
     }
 
     public static void isNotExistService(String service) throws ValidationException {
-        if (!servicesRepository.isExist(service))
+        if (!baseServiceRepository.isExist(service))
             throw new ValidationException("this Service is not Exist!");
     }
 }
