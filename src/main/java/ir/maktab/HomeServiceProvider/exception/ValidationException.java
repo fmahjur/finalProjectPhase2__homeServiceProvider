01e29a0b4626e0717aaf01fc9e4092edcb6e902a
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-25 02:08:44 +0330

    add ValidationException class

---
+++ src/main/java/ir/maktab/HomeServiceProvider/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package ir.maktab.HomeServiceProvider.exception;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
