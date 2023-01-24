1c90ad0399ef4e9dc0671942347fd5400d7db0cc
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-25 02:03:52 +0330

    update ExpertValidator class

--- src/main/java/ir/maktab/HomeServiceProvider/validation/ExpertValidator.java
+++ src/main/java/ir/maktab/HomeServiceProvider/validation/ExpertValidator.java
@@ -1,8 +1,10 @@
 package ir.maktab.HomeServiceProvider.validation;
 
+import ir.maktab.HomeServiceProvider.data.enums.ExpertStatus;
 import ir.maktab.HomeServiceProvider.data.model.Expert;
 import ir.maktab.HomeServiceProvider.data.model.SubService;
 import ir.maktab.HomeServiceProvider.data.repository.ExpertRepository;
+import ir.maktab.HomeServiceProvider.exception.NotFoundException;
 import ir.maktab.HomeServiceProvider.exception.ValidationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -13,12 +15,15 @@ public class ExpertValidator {
     private static ExpertRepository expertRepository;
 
     public static void isExistExpert(String email) throws ValidationException {
-        if (expertRepository.isExist(email))
+        if (expertRepository.findByEmailAddress(email) != null)
             throw new ValidationException("this Email is already Exist!");
     }
 
     public static void isExpertConfirmed(String username) throws ValidationException {
-        if (expertRepository.isExist(username))
+        Expert expertByUsername = expertRepository.findByUsername(username);
+        if (expertByUsername == null)
+            throw new NotFoundException("Not Found this Expert!");
+        else if (!expertByUsername.getExpertStatus().equals(ExpertStatus.ACCEPTED))
             throw new ValidationException("this Expert is not confirmed!");
     }
 
