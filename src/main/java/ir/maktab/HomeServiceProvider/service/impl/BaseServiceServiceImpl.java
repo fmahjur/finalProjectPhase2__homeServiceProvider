b10efb8ed756a363c26fe2be52ba84aef173270e
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-25 00:44:44 +0330

    add Methods body in BaseServiceService class

--- src/main/java/ir/maktab/HomeServiceProvider/service/impl/BaseServiceServiceImpl.java
+++ src/main/java/ir/maktab/HomeServiceProvider/service/impl/BaseServiceServiceImpl.java
@@ -1,7 +1,9 @@
 package ir.maktab.HomeServiceProvider.service.impl;
 
 import ir.maktab.HomeServiceProvider.data.model.BaseService;
+import ir.maktab.HomeServiceProvider.data.repository.BaseServiceRepository;
 import ir.maktab.HomeServiceProvider.service.BaseServiceService;
+import ir.maktab.HomeServiceProvider.validation.BaseServiceValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,23 +12,31 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class BaseServiceServiceImpl implements BaseServiceService {
+    private final BaseServiceRepository baseServiceRepository;
+
     @Override
     public void add(BaseService baseService) {
-
+        BaseServiceValidator.isExistService(baseService.getName());
+        baseServiceRepository.save(baseService);
     }
 
     @Override
     public void delete(BaseService baseService) {
-
+        baseService.setDeleted(true);
+        update(baseService);
     }
 
     @Override
     public void update(BaseService baseService) {
-
+        BaseService existingBaseService = baseServiceRepository.findById(baseService.getId()).orElse(null);
+        existingBaseService.setName(baseService.getName());
+        existingBaseService.setDeleted(baseService.isDeleted());
+        existingBaseService.setSubServiceList(baseService.getSubServiceList());
+        baseServiceRepository.save(existingBaseService);
     }
 
     @Override
     public List<BaseService> selectAll() {
-        return null;
+        return baseServiceRepository.findAll();
     }
 }
