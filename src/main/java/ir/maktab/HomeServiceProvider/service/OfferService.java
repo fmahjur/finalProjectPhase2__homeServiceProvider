d8e61902957a2d3e268100d2aedd8d3aaeaba795
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-24 23:53:37 +0330

    add OfferService interface

---
+++ src/main/java/ir/maktab/HomeServiceProvider/service/OfferService.java
@@ -0,0 +1,6 @@
+package ir.maktab.HomeServiceProvider.service;
+
+import ir.maktab.HomeServiceProvider.data.model.Offer;
+
+public interface OfferService extends MainService<Offer> {
+}
---
+++ src/main/java/ir/maktab/HomeServiceProvider/service/impl/OfferServiceImpl.java
@@ -0,0 +1,32 @@
+package ir.maktab.HomeServiceProvider.service.impl;
+
+import ir.maktab.HomeServiceProvider.data.model.Offer;
+import ir.maktab.HomeServiceProvider.service.OfferService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OfferServiceImpl implements OfferService {
+    @Override
+    public void add(Offer offer) {
+
+    }
+
+    @Override
+    public void delete(Offer offer) {
+
+    }
+
+    @Override
+    public void update(Offer offer) {
+
+    }
+
+    @Override
+    public List<Offer> selectAll() {
+        return null;
+    }
+}
