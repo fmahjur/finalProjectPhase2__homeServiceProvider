5869545233c4202e1edae1bd85baa2d32d72b51e
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-25 00:29:04 +0330

    add Methods body in AdminService class

--- src/main/java/ir/maktab/HomeServiceProvider/service/impl/AdminServiceImpl.java
+++ src/main/java/ir/maktab/HomeServiceProvider/service/impl/AdminServiceImpl.java
@@ -1,68 +1,79 @@
 package ir.maktab.HomeServiceProvider.service.impl;
 
+import ir.maktab.HomeServiceProvider.data.enums.ExpertStatus;
 import ir.maktab.HomeServiceProvider.data.model.BaseService;
 import ir.maktab.HomeServiceProvider.data.model.Expert;
 import ir.maktab.HomeServiceProvider.data.model.SubService;
 import ir.maktab.HomeServiceProvider.service.AdminService;
+import ir.maktab.HomeServiceProvider.validation.ExpertValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AdminServiceImpl implements AdminService {
+    private final BaseServiceServiceImpl baseServiceService;
+    private final SubServiceServiceImpl subServiceService;
+    private final ExpertServiceImpl expertService;
+    private final CustomerServiceImpl customerService;
 
     @Override
-    public void addNewService(BaseService service) {
-
+    public void addNewService(BaseService baseService) {
+        baseServiceService.add(baseService);
     }
 
     @Override
     public void addNewSubService(SubService subService) {
-
+        subServiceService.add(subService);
     }
 
     @Override
     public void confirmExpert(Expert expert) {
-
+        expert.setExpertStatus(ExpertStatus.ACCEPTED);
+        expertService.update(expert);
     }
 
     @Override
     public void allocationServiceToExpert(Expert expert, SubService subService) {
-
+        ExpertValidator.isExpertConfirmed(expert.getUsername());
+        expert.setSubServices(subService);
+        expertService.update(expert);
     }
 
     @Override
     public void removeExpertFromService(Expert expert, SubService subService) {
-
+        ExpertValidator.hasASubService(expert, subService);
+        expert.getSubServices().remove(subService);
+        expertService.update(expert);
     }
 
     @Override
     public void showAllService() {
-
+        baseServiceService.selectAll();
     }
 
     @Override
     public void showSubServices(BaseService service) {
-
+        subServiceService.getSubServicesByService(service);
     }
 
     @Override
     public void showAllCustomer() {
-
+        customerService.selectAll();
     }
 
     @Override
     public void showAllExpert() {
-
+        expertService.selectAll();
     }
 
     @Override
     public void showAllNewExpert() {
-
+        expertService.selectExpertByExpertStatus(ExpertStatus.NEW);
     }
 
     @Override
     public void showAllConfirmedExpert() {
-
+        expertService.selectExpertByExpertStatus(ExpertStatus.ACCEPTED);
     }
 }
