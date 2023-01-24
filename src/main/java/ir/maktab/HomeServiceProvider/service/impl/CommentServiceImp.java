5105d63ae669c8d49bf7b12832f05db4daef343e
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-25 00:49:58 +0330

    add Methods body in CommentService class

--- src/main/java/ir/maktab/HomeServiceProvider/service/impl/CommentServiceImp.java
+++ src/main/java/ir/maktab/HomeServiceProvider/service/impl/CommentServiceImp.java
@@ -1,6 +1,7 @@
 package ir.maktab.HomeServiceProvider.service.impl;
 
 import ir.maktab.HomeServiceProvider.data.model.Comment;
+import ir.maktab.HomeServiceProvider.data.repository.CommentRepository;
 import ir.maktab.HomeServiceProvider.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,23 +11,31 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class CommentServiceImp implements CommentService {
+    private final CommentRepository commentRepository;
+
     @Override
     public void add(Comment comment) {
-
+        commentRepository.save(comment);
     }
 
     @Override
     public void delete(Comment comment) {
-
+        comment.setDeleted(true);
+        update(comment);
     }
 
     @Override
     public void update(Comment comment) {
-
+        Comment existingComment = commentRepository.findById(comment.getId()).orElse(null);
+        existingComment.setComment(comment.getComment());
+        existingComment.setScore(comment.getScore());
+        existingComment.setExpert(comment.getExpert());
+        existingComment.setDeleted(comment.isDeleted());
+        commentRepository.save(existingComment);
     }
 
     @Override
     public List<Comment> selectAll() {
-        return null;
+        return commentRepository.findAll();
     }
 }
