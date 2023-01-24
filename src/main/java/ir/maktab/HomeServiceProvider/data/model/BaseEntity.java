11f9f7ad908501583582f3d216cfb70f622613cc
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-25 00:44:17 +0330

    updte BaseEntity class

--- src/main/java/ir/maktab/HomeServiceProvider/data/model/BaseEntity.java
+++ src/main/java/ir/maktab/HomeServiceProvider/data/model/BaseEntity.java
@@ -4,7 +4,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @MappedSuperclass
 public class BaseEntity {
     @Id
