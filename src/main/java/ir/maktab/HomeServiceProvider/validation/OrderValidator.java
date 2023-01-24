20e6138d648894115537a1c68c9569ad6b4334a9
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-25 00:18:42 +0330

    add OrderValidator class

--- src/main/java/ir/maktab/HomeServiceProvider/validation/OrderValidator.java
+++ src/main/java/ir/maktab/HomeServiceProvider/validation/OrderValidator.java
@@ -1,19 +1,23 @@
 package ir.maktab.HomeServiceProvider.validation;
 
+import ir.maktab.HomeServiceProvider.data.model.Order;
 import ir.maktab.HomeServiceProvider.exception.ValidationException;
-import ir.maktab.HomeServiceProvider.model.entity.Orders;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 import java.time.LocalDate;
 import java.util.Date;
 
+@Component
+@RequiredArgsConstructor
 public class OrderValidator {
     public static void isValidOrderStartDate(Date workStartDate) throws ValidationException {
         if (!workStartDate.before(Date.from(Instant.from(LocalDate.now()))))
             throw new ValidationException("work can not start before now!");
     }
 
-    public static void isValidCustomerProposedPrice(Orders order) throws ValidationException {
+    public static void isValidCustomerProposedPrice(Order order) throws ValidationException {
         if (!(order.getCustomerProposedPrice() >= order.getSubService().getBasePrice()))
             throw new ValidationException("work can not start before now!");
     }
