9bd37f2c3c16bf1e35190fe6e34a80c82486bc64
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-25 15:07:58 +0330

    update SubServiceRepository class

--- src/main/java/ir/maktab/HomeServiceProvider/data/repository/SubServiceRepository.java
+++ src/main/java/ir/maktab/HomeServiceProvider/data/repository/SubServiceRepository.java
@@ -1,7 +1,13 @@
 package ir.maktab.HomeServiceProvider.data.repository;
 
+import ir.maktab.HomeServiceProvider.data.model.BaseService;
 import ir.maktab.HomeServiceProvider.data.model.SubService;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SubServiceRepository extends JpaRepository<SubService, Long> {
+    SubService findByName(String subService);
+
+    List<SubService> findAllByBaseService(BaseService baseService);
 }
