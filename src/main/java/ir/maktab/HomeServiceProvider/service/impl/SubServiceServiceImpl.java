693613727dfa05b5d3b5793fca8dfa3376815df9
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-25 14:11:57 +0330

    add Methods body in SubServiceService class

--- src/main/java/ir/maktab/HomeServiceProvider/service/impl/SubServiceServiceImpl.java
+++ src/main/java/ir/maktab/HomeServiceProvider/service/impl/SubServiceServiceImpl.java
@@ -2,7 +2,10 @@ package ir.maktab.HomeServiceProvider.service.impl;
 
 import ir.maktab.HomeServiceProvider.data.model.BaseService;
 import ir.maktab.HomeServiceProvider.data.model.SubService;
+import ir.maktab.HomeServiceProvider.data.repository.SubServiceRepository;
 import ir.maktab.HomeServiceProvider.service.SubServiceService;
+import ir.maktab.HomeServiceProvider.validation.BaseServiceValidator;
+import ir.maktab.HomeServiceProvider.validation.SubServiceValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,28 +14,33 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class SubServiceServiceImpl implements SubServiceService {
+    private final SubServiceRepository subServiceRepository;
+
     @Override
     public void add(SubService subService) {
-
+        BaseServiceValidator.isNotExistService(subService.getBaseService().getName());
+        SubServiceValidator.isExistSubService(subService.getName());
+        subServiceRepository.save(subService);
     }
 
     @Override
     public void delete(SubService subService) {
-
+        subService.setDeleted(true);
+        subServiceRepository.save(subService);
     }
 
     @Override
     public void update(SubService subService) {
-
+        subServiceRepository.save(subService);
     }
 
     @Override
     public List<SubService> selectAll() {
-        return null;
+        return subServiceRepository.findAll();
     }
 
     @Override
     public List<SubService> getSubServicesByService(BaseService service) {
-        return null;
+        return subServiceRepository.findAllByBaseService(service);
     }
 }
