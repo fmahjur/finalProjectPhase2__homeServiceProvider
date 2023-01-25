8852ee27563bec8b948ebfca9d492b076656e6a5
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-25 14:07:01 +0330

    add Methods body in OrderService class

--- src/main/java/ir/maktab/HomeServiceProvider/service/OrderService.java
+++ src/main/java/ir/maktab/HomeServiceProvider/service/OrderService.java
@@ -1,9 +1,13 @@
 package ir.maktab.HomeServiceProvider.service;
 
+import ir.maktab.HomeServiceProvider.data.model.Customer;
 import ir.maktab.HomeServiceProvider.data.model.Order;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderService extends MainService<Order> {
-    public Optional<Order> getOrderDetail(String orderNumber);
+    List<Order> selectAllCustomersOrders(Customer customer);
+
+    Order getOrderDetail(String orderNumber);
 }
--- src/main/java/ir/maktab/HomeServiceProvider/service/impl/OrderServiceImpl.java
+++ src/main/java/ir/maktab/HomeServiceProvider/service/impl/OrderServiceImpl.java
@@ -1,7 +1,10 @@
 package ir.maktab.HomeServiceProvider.service.impl;
 
+import ir.maktab.HomeServiceProvider.data.model.Customer;
 import ir.maktab.HomeServiceProvider.data.model.Order;
+import ir.maktab.HomeServiceProvider.data.repository.OrderRepository;
 import ir.maktab.HomeServiceProvider.service.OrderService;
+import ir.maktab.HomeServiceProvider.validation.OrderValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,28 +14,37 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
+    private final OrderRepository orderRepository;
     @Override
     public void add(Order order) {
-
+        OrderValidator.isValidOrderStartDate(order.getWorkStartDate());
+        OrderValidator.isValidCustomerProposedPrice(order);
+        orderRepository.save(order);
     }
 
     @Override
     public void delete(Order order) {
-
+        order.setDeleted(true);
+        orderRepository.save(order);
     }
 
     @Override
     public void update(Order order) {
-
+        orderRepository.save(order);
     }
 
     @Override
     public List<Order> selectAll() {
-        return null;
+        return orderRepository.findAll();
     }
 
     @Override
-    public Optional<Order> getOrderDetail(String orderNumber) {
-        return Optional.empty();
+    public List<Order> selectAllCustomersOrders(Customer customer) {
+        return orderRepository.findAllByCustomer(customer);
+    }
+
+    @Override
+    public Order getOrderDetail(String orderNumber) {
+        return orderRepository.findByOrderNumber(orderNumber);
     }
 }
