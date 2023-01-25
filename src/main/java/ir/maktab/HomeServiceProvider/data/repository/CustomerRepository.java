007bbdd70db4af26440722ea26ed2236495be329
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-25 14:13:52 +0330

    update CustomerRepository class

--- src/main/java/ir/maktab/HomeServiceProvider/data/repository/CustomerRepository.java
+++ src/main/java/ir/maktab/HomeServiceProvider/data/repository/CustomerRepository.java
@@ -4,5 +4,7 @@ import ir.maktab.HomeServiceProvider.data.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Customer findByUsername(String username);
+    Customer findByEmailAddress(String emailAddress);
 
 }
