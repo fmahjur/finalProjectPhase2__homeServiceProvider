4d6bc3065fc6b1edd3b1c5b270e7befba209ab69
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-25 15:07:26 +0330

    update OrderRepository class

--- src/main/java/ir/maktab/HomeServiceProvider/data/repository/OrderRepository.java
+++ src/main/java/ir/maktab/HomeServiceProvider/data/repository/OrderRepository.java
@@ -1,9 +1,14 @@
 package ir.maktab.HomeServiceProvider.data.repository;
 
+import ir.maktab.HomeServiceProvider.data.model.Customer;
 import ir.maktab.HomeServiceProvider.data.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findAllByCustomer(Customer customer);
+    Order findByOrderNumber(String orderNumber);
 }
