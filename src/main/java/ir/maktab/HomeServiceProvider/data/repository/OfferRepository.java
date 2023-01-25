b8fcbc8ca2dab702fb133519a0d048ce2215bfcb
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-25 14:14:48 +0330

    update OfferRepository class

--- src/main/java/ir/maktab/HomeServiceProvider/data/repository/OfferRepository.java
+++ src/main/java/ir/maktab/HomeServiceProvider/data/repository/OfferRepository.java
@@ -1,10 +1,16 @@
 package ir.maktab.HomeServiceProvider.data.repository;
 
+import ir.maktab.HomeServiceProvider.data.model.Expert;
 import ir.maktab.HomeServiceProvider.data.model.Offer;
+import ir.maktab.HomeServiceProvider.data.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OfferRepository extends JpaRepository<Offer, Long> {
+    List<Offer> findAllByOrder(Order order);
 
+    List<Offer> findAllByExpert(Expert expert);
 }
