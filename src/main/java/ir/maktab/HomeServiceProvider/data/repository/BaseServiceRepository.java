ff5680b935f3eff0c49ed3ed5b79e987cf8e80d1
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-25 14:13:29 +0330

    update BaseServiceRepository class

--- src/main/java/ir/maktab/HomeServiceProvider/data/repository/BaseServiceRepository.java
+++ src/main/java/ir/maktab/HomeServiceProvider/data/repository/BaseServiceRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BaseServiceRepository extends JpaRepository<BaseService, Long> {
+    BaseService findByName(String baseServiceName);
 }
