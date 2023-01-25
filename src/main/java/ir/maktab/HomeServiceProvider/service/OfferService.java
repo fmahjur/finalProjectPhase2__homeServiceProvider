91c93b728a36fcf47fefb1ffa5d26da89b842672
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-25 13:58:29 +0330

    add Methods body in OfferService class

--- src/main/java/ir/maktab/HomeServiceProvider/service/OfferService.java
+++ src/main/java/ir/maktab/HomeServiceProvider/service/OfferService.java
@@ -1,6 +1,12 @@
 package ir.maktab.HomeServiceProvider.service;
 
+import ir.maktab.HomeServiceProvider.data.model.Expert;
 import ir.maktab.HomeServiceProvider.data.model.Offer;
+import ir.maktab.HomeServiceProvider.data.model.Order;
+
+import java.util.List;
 
 public interface OfferService extends MainService<Offer> {
+    List<Offer> selectAllByOrder(Order order);
+    List<Offer> selectAllByExpert(Expert expert);
 }
--- src/main/java/ir/maktab/HomeServiceProvider/service/impl/OfferServiceImpl.java
+++ src/main/java/ir/maktab/HomeServiceProvider/service/impl/OfferServiceImpl.java
@@ -1,6 +1,9 @@
 package ir.maktab.HomeServiceProvider.service.impl;
 
+import ir.maktab.HomeServiceProvider.data.model.Expert;
 import ir.maktab.HomeServiceProvider.data.model.Offer;
+import ir.maktab.HomeServiceProvider.data.model.Order;
+import ir.maktab.HomeServiceProvider.data.repository.OfferRepository;
 import ir.maktab.HomeServiceProvider.service.OfferService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,23 +13,35 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class OfferServiceImpl implements OfferService {
+    private final OfferRepository offerRepository;
+
     @Override
     public void add(Offer offer) {
-
+        offerRepository.save(offer);
     }
 
     @Override
     public void delete(Offer offer) {
-
+        offer.setDeleted(true);
+        offerRepository.save(offer);
     }
 
     @Override
     public void update(Offer offer) {
-
+        offerRepository.save(offer);
     }
 
     @Override
     public List<Offer> selectAll() {
-        return null;
+        return offerRepository.findAll();
+    }
+
+    @Override
+    public List<Offer> selectAllByOrder(Order order) {
+        return offerRepository.findAllByOrder(order);
+    }
+
+    public List<Offer> selectAllByExpert(Expert expert) {
+        return offerRepository.findAllByExpert(expert);
     }
 }
