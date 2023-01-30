d2ea467e259e1fd48bafb3ed22fa641b0a1ca962
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-30 15:32:35 +0330

    update MainService class

--- src/main/java/ir/maktab/HomeServiceProvider/service/MainService.java
+++ src/main/java/ir/maktab/HomeServiceProvider/service/MainService.java
@@ -3,11 +3,11 @@ package ir.maktab.HomeServiceProvider.service;
 import java.util.List;
 
 public interface MainService<T> {
-    void add(T t);
+    T add(T t);
 
     void delete(T t);
 
-    void update(T t);
+    T update(T t);
 
     List<T> selectAll();
 }
