38550b0088004cdb8d79d5f9c1444bacb38819bb
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-30 15:33:16 +0330

    add OfferValidator class

---
+++ src/main/java/ir/maktab/HomeServiceProvider/validation/OfferValidator.java
@@ -0,0 +1,17 @@
+package ir.maktab.HomeServiceProvider.validation;
+
+import ir.maktab.HomeServiceProvider.data.model.Offer;
+import ir.maktab.HomeServiceProvider.exception.IncorrectInformationException;
+import ir.maktab.HomeServiceProvider.exception.ValidationException;
+
+public class OfferValidator {
+    public static void isValidExpertProposedPrice(Offer offer) throws ValidationException {
+        if (!(offer.getOfferPrice() >= offer.getOrder().getSubService().getBasePrice()))
+            throw new ValidationException("The proposed price cannot be less than the base price!");
+    }
+
+    public static void hasDurationOfWork(Offer offer){
+        if (offer.getDurationOfWork() == null)
+            throw new IncorrectInformationException("Your offer must include the duration of the work");
+    }
+}
