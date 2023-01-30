5f21739c3c42c7593910c4f5fe55016ab4f266c1
Reihaneh Mahjour <rihaneh763@gmail.com>
2023-01-30 15:32:01 +0330

    update method: selectAllByOrder to OfferService class

--- src/main/java/ir/maktab/HomeServiceProvider/service/impl/OfferServiceImpl.java
+++ src/main/java/ir/maktab/HomeServiceProvider/service/impl/OfferServiceImpl.java
@@ -8,6 +8,8 @@ import ir.maktab.HomeServiceProvider.service.OfferService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -16,8 +18,8 @@ public class OfferServiceImpl implements OfferService {
     private final OfferRepository offerRepository;
 
     @Override
-    public void add(Offer offer) {
-        offerRepository.save(offer);
+    public Offer add(Offer offer) {
+        return offerRepository.save(offer);
     }
 
     @Override
@@ -27,8 +29,8 @@ public class OfferServiceImpl implements OfferService {
     }
 
     @Override
-    public void update(Offer offer) {
-        offerRepository.save(offer);
+    public Offer update(Offer offer) {
+        return offerRepository.save(offer);
     }
 
     @Override
@@ -38,7 +40,22 @@ public class OfferServiceImpl implements OfferService {
 
     @Override
     public List<Offer> selectAllByOrder(Order order) {
-        return offerRepository.findAllByOrder(order);
+        List<Offer> allOfferForOrder = offerRepository.findAllByOrder(order);
+        Collections.sort(allOfferForOrder, new Comparator() {
+            public int compare(Object o1, Object o2) {
+                Double x1 = ((Offer) o1).getOfferPrice();
+                Double x2 = ((Offer) o2).getOfferPrice();
+                int compare = x1.compareTo(x2);
+                if (compare != 0) {
+                    return compare;
+                }
+
+                Double y1 = ((Offer) o1).getExpert().getRate();
+                Double y2 = ((Offer) o2).getExpert().getRate();
+                return y1.compareTo(y2);
+            }
+        });
+        return allOfferForOrder;
     }
 
     public List<Offer> selectAllByExpert(Expert expert) {
