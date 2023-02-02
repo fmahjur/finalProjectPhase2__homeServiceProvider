3b8d90341ee84af9ad6ee61e141c523a47fd0481
reihaneh <reihaneh763@gmail.com>
2023-02-02 20:23:56 +0330

    change Order class name to Orders

---
+++ src/main/java/ir/maktab/HomeServiceProvider/data/model/Orders.java
@@ -0,0 +1,61 @@
+package ir.maktab.HomeServiceProvider.data.model;
+
+import ir.maktab.HomeServiceProvider.data.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class Orders extends BaseEntity {
+    String orderNumber;
+    @ManyToOne
+    Customer customer;
+
+    @ManyToOne
+    SubService subService;
+
+    @OneToMany(mappedBy = "orders")
+    Set<Offer> offers;
+
+    @Column(nullable = false)
+    String description;
+
+    @Column(nullable = false)
+    Long CustomerProposedPrice;
+
+    @Enumerated(EnumType.STRING)
+    OrderStatus orderStatus;
+
+    @OneToOne
+    Comment comment;
+
+    @Column(columnDefinition = "boolean default false")
+    boolean isDeleted;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    Date orderRegistrationDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    Date workStartDate;
+
+    DurationOfWork durationOfWork;
+
+    @OneToOne
+    Address address;
+
+    public Orders() {
+        this.orderStatus = OrderStatus.WAITING_FOR_EXPERTS_OFFER;
+    }
+}
