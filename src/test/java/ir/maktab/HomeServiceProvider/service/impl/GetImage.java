c1948f6278ee5566b8b71439a803bd5307a633c6
reihaneh <reihaneh763@gmail.com>
2023-02-03 18:35:32 +0330

    add GetImage class

--- src/test/java/ir/maktab/HomeServiceProvider/service/impl/GetImage.java
+++ src/test/java/ir/maktab/HomeServiceProvider/service/impl/GetImage.java
@@ -1,2 +1,26 @@
-package ir.maktab.HomeServiceProvider.service.impl;public class GetImage {
+package ir.maktab.HomeServiceProvider.service.impl;
+
+import ir.maktab.HomeServiceProvider.validation.PictureValidator;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class GetImage {
+    public byte[] getImage() {
+        String imageFilePath = "C:\\Users\\paage\\OneDrive\\Documents\\reihaneh\\1111.jpg";
+        PictureValidator.isValidImageFile(imageFilePath);
+        File file = new File(imageFilePath);
+        byte[] bFile = new byte[(int) file.length()];
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            fileInputStream.read(bFile);
+            fileInputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return bFile;
+    }
 }
