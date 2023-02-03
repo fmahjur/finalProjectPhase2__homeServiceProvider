0ba1e96fafdc99ae930e66ed7fa0d18206229ca8
reihaneh <reihaneh763@gmail.com>
2023-02-03 16:03:13 +0330

    add CommentServiceTest class

--- src/test/java/ir/maktab/HomeServiceProvider/service/impl/CommentServiceImpTest.java
+++ src/test/java/ir/maktab/HomeServiceProvider/service/impl/CommentServiceImpTest.java
@@ -3,17 +3,17 @@ package ir.maktab.HomeServiceProvider.service.impl;
 import ir.maktab.HomeServiceProvider.data.model.Comment;
 import ir.maktab.HomeServiceProvider.data.model.Credit;
 import ir.maktab.HomeServiceProvider.data.model.Expert;
+import ir.maktab.HomeServiceProvider.data.repository.CommentRepository;
 import ir.maktab.HomeServiceProvider.validation.PictureValidator;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -25,54 +25,47 @@ class CommentServiceImpTest {
     private CommentServiceImp commentService;
 
     @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
     private ExpertServiceImpl expertService;
 
-    Expert expert = new Expert("reihan", "mahjour", "reihaneh763@gmail.com", "Rm@123456", new Credit(1000L), getImage());
-    Expert expert1 = new Expert("hamid", "mahjour", "hamid@gmail.com", "Hm@123456", new Credit(1000L), getImage());
+    GetImage image = new GetImage();
+
+    Expert expert = new Expert("reihan", "mahjour", "kaveh@gmail.com", "Rm@123456", new Credit(1000L), image.getImage());
+    Expert expert1 = new Expert("hamid", "mahjour", "mohammad@gmail.com", "Hm@123456", new Credit(1000L), image.getImage());
+    Expert expert2 = new Expert("hamid", "mahjour", "ali@gmail.com", "Am@123456", new Credit(1000L), image.getImage());
     Comment comment = new Comment("Comment", 5.0, expert, false);
     Comment comment1 = new Comment("Comment1", 4.0, expert1, false);
+    Comment comment2 = new Comment("Comment2", 3.0, expert2, false);
 
     @Test
-    void add() {
-        expertService.add(expert);
+    void addNewCommentTest() {
         Comment saveComment = commentService.add(comment);
         assertNotNull(saveComment);
     }
 
     @Test
-    void remove() {
-        commentService.remove(comment);
+    void testSoftDeleteComment() {
+        Comment saveComment = commentService.add(comment2);
+        commentService.remove(saveComment);
+        Optional<Comment> optionalComment = commentRepository.findById(saveComment.getId());
+        assertThat(optionalComment.get().isDeleted()).isEqualTo(true);
+
     }
 
     @Test
-    void update() {
-        expertService.add(expert1);
-        commentService.add(comment1);
-        comment1.setComment("update comment1");
-        Comment updateComment = commentService.update(comment1);
+    void testUpdateComment() {
+        Comment saveComment = commentService.add(comment1);
+        saveComment.setComment("update comment1");
+        Comment updateComment = commentService.update(saveComment);
         assertThat(updateComment.getComment()).isEqualTo("update comment1");
     }
 
     @Test
-    void selectAll() {
+    void testSelectAllComment() {
         List<Comment> commentList = commentService.selectAllAvailableService();
         assertThat(commentList).isNotNull();
-        assertThat(commentList.size()).isEqualTo(1);
-    }
-
-    private byte[] getImage() {
-        String imageFilePath = "C:\\Users\\paage\\OneDrive\\Documents\\reihaneh\\1111.jpg";
-        PictureValidator.isValidImageFile(imageFilePath);
-        File file = new File(imageFilePath);
-        byte[] bFile = new byte[(int) file.length()];
-        try {
-            FileInputStream fileInputStream = new FileInputStream(file);
-            fileInputStream.read(bFile);
-            fileInputStream.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return bFile;
+        assertThat(commentList.size()).isEqualTo(2);
     }
 }
